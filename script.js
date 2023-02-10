5f049a9fdec505f47ac293562453272849e31c72
Jenally Castaneda <castaneda.jenally@gmail.com>
2023-02-09 22:06:05 -0500

    HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,12 @@
+function writepassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+
+    passwordText.value = password;
+}
+
+generateBtn = document.querySelector('#generate');
+
+function writePassword() {
+    var password = generatePassword
+}
